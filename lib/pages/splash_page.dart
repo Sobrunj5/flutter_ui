c65b7857db1ab14ec0bebf311aa0119202abc74e
SasqiaAyu <sasqia.ayul@gmail.com>
2021-03-21 19:31:45 +0700

    update

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:student_ui/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage('assets/logo.png'),
+              )),
+            ),
+            SizedBox(
+              height: 96,
+            ),
+            Container(
+              width: 276,
+              height: 200,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage('assets/Scenes.png'),
+              )),
+            ),
+            SizedBox(height: 56),
+            Text(
+              "Up Your Skill",
+              style: blackTextStyle.copyWith(fontSize: 28),
+            ),
+            SizedBox(
+              height: 7,
+            ),
+            Text(
+              'We provide content that helps\neveryone to learn anything',
+              textAlign: TextAlign.center,
+              style: greyTextStyle.copyWith(fontSize: 20),
+            ),
+            SizedBox(
+              height: 96,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - (2 * defaultMargin),
+              height: 50,
+              child: FlatButton(
+                onPressed: () {},
+                color: blueColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(17),
+                ),
+                child: Text(
+                  'Get Started',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 22,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
