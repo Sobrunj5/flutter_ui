d70516d2072e2096f76ae4cfa6a885e1ea933f6f
SasqiaAyu <sasqia.ayul@gmail.com>
2021-04-20 08:49:30 +0700

    update

--- lib/pages/course_page.dart
+++ lib/pages/course_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:student_ui/theme.dart';
+import 'package:student_ui/widgets/course_tile.dart';
 
 class CoursePage extends StatelessWidget {
   @override
@@ -27,8 +29,132 @@ class CoursePage extends StatelessWidget {
                 height: 225,
               ),
               Container(
+                padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(30),
+                  ),
+                  color: backgroundColor,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'Brand Marketing Design',
+                      style: blackTextStyle.copyWith(fontSize: 22),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      'How to build strong company with passion',
+                      style: greyTextStyle.copyWith(fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    // Note: PREPARING
 
+                    Text(
+                      '#Preparing',
+                      style: blackTextStyle.copyWith(fontSize: 18),
+                    ),
+                    CourseTile(
+                      imageUrl: 'assets/icon1.png',
+                      title: 'Ideaton',
+                      value: 0.7,
+                      isLocked: false,
+                    ),
+                    CourseTile(
+                      imageUrl: 'assets/icon2.png',
+                      title: 'Validate Idea',
+                      value: 0.2,
+                      isLocked: false,
+                    ),
+                    CourseTile(
+                      imageUrl: 'assets/icon3.png',
+                      title: 'Strong Promotion',
+                      value: 0.0,
+                      isLocked: true,
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+
+                    // Note: TARGETING CUSTOMER
+
+                    Text(
+                      '#Targer Customer',
+                      style: blackTextStyle.copyWith(fontSize: 18),
+                    ),
+                    CourseTile(
+                      imageUrl: 'assets/icon4.png',
+                      title: 'App Survey',
+                      value: 0.0,
+                      isLocked: true,
+                    ),
+                    CourseTile(
+                      imageUrl: 'assets/icon5.png',
+                      title: 'Get Funding',
+                      value: 0.0,
+                      isLocked: true,
+                    ),
+                    CourseTile(
+                      imageUrl: 'assets/icon6.png',
+                      title: 'Ship to Investor',
+                      value: 0.0,
+                      isLocked: true,
+                    ),
+
+                    SizedBox(
+                      height: 50,
+                    ),
+
+                    //NOTE:BUTTONS
+                    Container(
+                      width: MediaQuery.of(context).size.width -
+                          (2 * defaultMargin),
+                      height: 50,
+                      child: FlatButton(
+                        onPressed: () {},
+                        color: blueColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                        child: Text(
+                          'Finish and Take Quiz',
+                          style: whiteTextStyle.copyWith(fontSize: 22),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
+              SizedBox(
+                height: 15,
+              ),
+              
+               Container(
+                      width: MediaQuery.of(context).size.width -
+                          (2 * defaultMargin),
+                      height: 50,
+                      child: FlatButton(
+                        onPressed: () {},
+                        color: Color(0xffECEEF5),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                        child: Text(
+                          'Skip to Home',
+                          style: GoogleFonts.tajawal( color: Color(0xffC38DA), fontSize: 22),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 52,)
+
             ],
           )
         ],
